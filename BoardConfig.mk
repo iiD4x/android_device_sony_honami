8ff75006bb439eabc81c0d2a91bcb570d90d90ea
iiD4x <t2pksa@gmail.com>
2014-08-06 01:39:28 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,8 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/honami/bluetooth
 
 # Kernel properties
 TARGET_KERNEL_CONFIG := cm_rhine_honami_row_defconfig
+TARGET_GCC_VERSION_ARM := 4.9-sm
+TARGET_GCC_VERSION_AND := 4.8-sm
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 26
